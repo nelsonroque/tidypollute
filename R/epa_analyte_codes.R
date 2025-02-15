f4f22f79786cb5b6e845ae17ca1f468e20917608
Nelson Roque <nelsonroquejr@gmail.com>
2025-02-14 21:46:30 -0500

    Auto-update before build

--- R/epa_analyte_codes.R
+++ R/epa_analyte_codes.R
@@ -1,8 +1,6 @@
 #' EPA AirData Analyte Codes
 #'
-#' This dataset contains metadata for all downloadable ZIP files from the EPA AirData website.
-#' The metadata includes file links, year, unit of analysis, analyte type, and human-readable descriptions.
-#'
+#' This dataset contains the queryable names for EPA analytes from the EPA AirData website.
 #' @format A tibble with the following columns:
 #' \describe{
 #'   \item{analyte}{The pollutant or air quality measure extracted from the filename.}
@@ -10,5 +8,5 @@
 #' @source \url{https://aqs.epa.gov/aqsweb/airdata/download_files.html}
 #' @examples
 #' data(epa_analyte_codes)
-#' head(epa_analyte_codes)
+#' epa_analyte_codes
 "epa_analyte_codes"
